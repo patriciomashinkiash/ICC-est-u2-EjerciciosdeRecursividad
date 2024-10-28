11eafa546424185d95dd5e8ffeb31044c5544fb6
PaulSebastianNaspud <naspud972012@gmaail.com>
2024-10-28 12:52:10 -0500

    first commit

--- src/App.java
+++ src/App.java
@@ -2,7 +2,13 @@ public class App {
     public static void main(String[] args) throws Exception {
         Recursividad rec = new Recursividad();
         int resultado = rec.factorial(5);
-        System.out.println("Resultado final: " + resultado);
+        System.out.println("Resultado final: " + resultado + "\n");
+
+        System.out.println("Resultado final: " + rec.sumaConsecutivos(5) + "\n");
+
+        System.out.println("Resultado final: " + rec.potencia(5, 3) + "\n");
+
+        System.out.println("Resultado final : " + rec.sumaDigitos(234) + "\n");
 
     }
 }
--- src/Recursividad.java
+++ src/Recursividad.java
@@ -4,7 +4,7 @@ public class Recursividad {
         System.out.println("Calculando el factorial de: " + n);
         // caso base: n sea 0! y 1! son iguales a 1
         if (n == 0 || n == 1) {
-            System.out.println("Caso base alcanzado el factorial de" + n + "es 1");
+            System.out.println("Caso base alcanzado el factorial de" + n + " es 1");
             return 1;
         }
 
@@ -12,4 +12,48 @@ public class Recursividad {
         System.out.println("Resultado parcial para " + n + " * factorial (" + (n - 1) + ") = " + resultado);
         return resultado;
     }
+
+    // CALCULAR LA SUMA DE LOS NUMEROS CONSECUTIVOS
+    // n = 5 resultado = 5+4+3+2+1 = 15
+    public int sumaConsecutivos(int n) {
+        System.out.println("Calculando la suma de los numeros consecutivos de: " + n);
+        if (n == 1) {
+            System.out.println("Caso base alcanzado el factorial de" + n + " es 1");
+            return 1;
+        }
+
+        int resultado = n + sumaConsecutivos(n - 1);
+        System.out.println("Resultado parcial para " + n + " + (" + (n - 1) + ") = " + resultado);
+        return resultado;
+    }
+
+    // CALCULA LA POTENCIA DE NUMERO
+    public int potencia(int base, int exponente) {
+        System.out.println("Calculando la potencia de " + base + " elevado a: " + exponente);
+        if (exponente == 0) {
+            return 1;
+        }
+
+        int resultado = base * potencia(base, exponente - 1);
+        System.out.println("Resultado parcial para " + base + "^" + (exponente) + " = " + resultado);
+        return resultado;
+    }
+
+    // CREAR UN METODO QUE SUME LOS DIGITOS DE UN NUMERO
+    // SI MANDO 456 SEA IGUAL A 4+5+6 = 15
+    // PISTA SE USA % MOD
+
+    public int sumaDigitos(int numero) {
+        System.out.println("Calculando la suma de los digitos de: " + numero);
+        if (numero < 10) {
+            System.out.println("Caso base alcanzado el factorial de" + numero + " es " + numero);
+            return numero;
+        }
+
+        int resultado = (numero % 10) + sumaDigitos(numero / 10);
+        System.out.println("Resultado parcial para " + numero + " % 10 + (" + (numero / 10) + ") = " + resultado);
+        return resultado;
+
+    }
+
 }
