e392c67cd9b20466ae95b1bf343b4f5323099eb2
PaulSebastianNaspud <naspud972012@gmaail.com>
2024-10-28 12:16:04 -0500

    first commit

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Recursividad rec = new Recursividad();
         int resultado = rec.factorial(5);
-        System.out.println(resultado);
+        System.out.println("Resultado final: " + resultado);
 
     }
 }
--- src/Recursividad.java
+++ src/Recursividad.java
@@ -1,11 +1,15 @@
 public class Recursividad {
 
     public int factorial(int n) {
+        System.out.println("Calculando el factorial de: " + n);
         // caso base: n sea 0! y 1! son iguales a 1
         if (n == 0 || n == 1) {
+            System.out.println("Caso base alcanzado el factorial de" + n + "es 1");
             return 1;
         }
 
-        return n * factorial(n - 1);
+        int resultado = n * factorial(n - 1);
+        System.out.println("Resultado parcial para " + n + " * factorial (" + (n - 1) + ") = " + resultado);
+        return resultado;
     }
 }
